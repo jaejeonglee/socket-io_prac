b2ad73c0fd1295cfe52148fe9273428f3dd59230
jaejeonglee <https://github.com/jaejeonglee>
2022-03-11 01:04:58 +0900

    소켓 기능  정리

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const http = require('http')
 const cors = require('cors')
 const { Server } = require('socket.io')
+const port = 3001
 
 app.use(cors())
 
@@ -23,20 +24,21 @@ io.on("connection", (socket) => {
     //whereRoom에 참가하기
     socket.on("join-room", (whereRoom) => {
         socket.join(whereRoom)
-        console.log(`User with ID: ${socket.id} joined room: ${whereRoom}`)
+        console.log(`User ID: ${socket.id} joined room: ${whereRoom}`)
     })
 
+    //whereRoom 내에서의 채팅로그
     socket.on("send_message", (chatLog) => {
         socket.to(chatLog.room).emit("receive_message", chatLog)
         console.log('chatLog:',chatLog)
     })
+
+    //클라이언트와 연결 종료
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id)
     })
 })
 
-
-
-server.listen(3001, () => {
-    console.log("SERVER RUNNING")
+server.listen(port, () => {
+    console.log(new Date().toLocaleTimeString(),port,'CONNECT')
 })
